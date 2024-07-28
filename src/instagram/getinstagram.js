33e93c29be6e475cedbfcdfa6293983cf3273f03
phillychi3 <phillychi3@gmail.com>
2024-07-28 21:23:14 +0800

    clean

--- src/instagram/getinstagram.js
+++
@@ -1,9 +0,0 @@
-//use python to get instagram data
-//pythonpath : ./lib/instagram/main.py
-
-var spawn = require('child_process').spawn;
-var path = require('path');
-var fs = require('fs');
-
-
-
--- src/twitter/gettwitter2.js
+++
@@ -1,7 +0,0 @@
-//use unofficial twitter api
-//https://github.com/DIYgod/RSSHub/blob/master/lib/v2/twitter/web-api/constants.js
-const request = require("request");
-
-module.exports = function gettwitter() {
-    
-}
